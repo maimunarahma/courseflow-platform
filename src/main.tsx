a35d95f84d2cee58d94ecc123dd728cc089d6db0
Maimuna Rahman <maimunarahman30@gmail.com>
2025-12-06 16:00:49 +0600

    lets see

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
 import { createRoot } from "react-dom/client";
+import axios from "axios";
 import App from "./App.tsx";
 import "./index.css";
+
+// Ensure axios sends credentials (cookies) with requests by default
+axios.defaults.withCredentials = true;
+// Optionally set baseURL if you use VITE_SERVER_URL env var
+if (import.meta.env.VITE_SERVER_URL) {
+	axios.defaults.baseURL = import.meta.env.VITE_SERVER_URL as string;
+}
 
 createRoot(document.getElementById("root")!).render(<App />);
